7361ce82442827cc3f9aa2ded337df8563c6ffd7
Lucas Grativol Ribeiro <lucasgrativolr@gmail.com>
2020-07-30 16:54:22 -0300

    move sizes contants to pkg

--- hdl/pkg/utils_pkg.vhd
+++ hdl/pkg/utils_pkg.vhd
@@ -5,13 +5,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
---use ieee.fixed_float_types.all; -- only synthesis
---use ieee.fixed_pkg.all;         -- only synthesis
 
-library ieee_proposed;                      -- only simulation
-use ieee_proposed.fixed_float_types.all;    -- only simulation
-use ieee_proposed.fixed_pkg.all;            -- only simulation
-
+library ieee_proposed;                      
+use ieee_proposed.fixed_float_types.all;
+use ieee_proposed.fixed_pkg.all; 
 
 package utils_pkg is
 
@@ -28,10 +25,19 @@ package utils_pkg is
     ---------------
     -- Constants --
     ---------------
+    
+    -- Cordic
+    constant CORDIC_INTEGER_PART    : natural  := 1;
+    constant N_CORDIC_ITERATIONS    : natural  := 21;
+    constant CORDIC_FRAC_PART       : integer  := -(N_CORDIC_ITERATIONS - (CORDIC_INTEGER_PART + 1));
+    
+    constant PHASE_INTEGER_PART     : natural  := 3;   -- for unsigned phase
+    constant PHASE_FRAC_PART        : integer  := -30; 
 
     -- Obs.: Using the fixed_pkg
-    constant PI_INTEGER_PART        : integer := 2;
+    constant PI_INTEGER_PART        : integer := 2; 
     constant PI_FRAC_PART           : integer := -30;
+
     constant PI                     : ufixed(PI_INTEGER_PART downto PI_FRAC_PART) := to_ufixed(MATH_PI, PI_INTEGER_PART,PI_FRAC_PART);
                              
 end utils_pkg;
