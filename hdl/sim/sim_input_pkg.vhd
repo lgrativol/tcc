0fde5b6115f4ff1d9a888d56104a3cc0e43c4ad9
Lucas Grativol Ribeiro <lucasgrativolr@gmail.com>
2020-08-10 19:03:43 -0300

    add new sim inputs

--- hdl/sim/sim_input_pkg.vhd
+++ hdl/sim/sim_input_pkg.vhd
@@ -2,6 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package sim_input_pkg is
-   constant SIM_INPUT_TARGETFREQ     : positive  := 100000;
-   constant SIM_INPUT_NBCYCLES       : natural   := 200;
+   constant SIM_INPUT_TARGETFREQ     : positive  := 500000;
+   constant SIM_INPUT_NBCYCLES       : natural   := 10;
+   constant SIM_INPUT_TX_TIME        : positive  := 2000;
+   constant SIM_INPUT_TX__OFF_TIME   : positive  := 30;
+   constant SIM_INPUT_RX_TIME        : positive  := 2000;
+   constant SIM_INPUT_OFF_TIME       : positive  := 20;
 end sim_input_pkg;
