95203dc414f9591c39caf02f4436a16317ceefcb
Lucas Grativol Ribeiro <lucasgrativolr@gmail.com>
2020-08-20 22:10:58 -0300

    add new sim input options

--- hdl/sim/sim_input_pkg.vhd
+++ hdl/sim/sim_input_pkg.vhd
@@ -5,12 +5,16 @@ library work;
 use work.utils_pkg.all;
 
 package sim_input_pkg is
-   constant SIM_INPUT_TARGETFREQ     : positive  := 500000;
-   constant SIM_INPUT_NBCYCLES       : natural   := 10;
-   constant SIM_INPUT_PHASE_DIFF     : std_logic_vector((PHASE_WIDTH - 1) downto 0) := x"0c90fdaa22";
-   constant SIM_INPUT_TX_TIME        : positive  := 2000;
+   -- DDS
+   constant SIM_INPUT_NBPOINTS       : natural   := 200;
+   constant SIM_INPUT_NBREPET        : natural   := 4;
+   constant SIM_INPUT_PHASE_TERM     : std_logic_vector((PHASE_WIDTH - 1) downto 0) := x"004056FE48";
+   constant SIM_INPUT_INIT_PHASE     : std_logic_vector((PHASE_WIDTH - 1) downto 0) := x"0000000000";
+   constant SIM_INPUT_MODE_TIME      : boolean   := False;
+   constant SIM_INPUT_EN_POSPROC     : boolean   := False;
+   -- Double Driver
+   constant SIM_INPUT_TX_TIME        : positive  := 400;
    constant SIM_INPUT_TX_OFF_TIME    : positive  := 300;
-   constant SIM_INPUT_RX_TIME        : positive  := 10000;
+   constant SIM_INPUT_RX_TIME        : positive  := 300;
    constant SIM_INPUT_OFF_TIME       : positive  := 100;
-   constant SIM_INPUT_MODE_TIME      : boolean   := True;
 end sim_input_pkg;
