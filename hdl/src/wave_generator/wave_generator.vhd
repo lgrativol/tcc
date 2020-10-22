6ead4e2ddae45048cbdcea8cdeea7f5896a75e95
Lucas Grativol Ribeiro <lucasgrativolr@gmail.com>
2020-10-22 17:38:58 -0300

    update to have all inputs at once

--- hdl/src/wave_generator/wave_generator.vhd
+++ hdl/src/wave_generator/wave_generator.vhd
@@ -13,13 +13,13 @@ use ieee_proposed.fixed_pkg.all;
 
 library work;
 use work.utils_pkg.all;
+
 ------------
 -- Entity --
 ------------
 
 entity wave_generator is
     generic(
-        CONFIG_DATA_WIDTH                   : positive;
         OUTPUT_WIDTH                        : positive
     );
     port(
@@ -29,11 +29,26 @@ entity wave_generator is
 
         -- Input interface
         bang_i                              : in  std_logic;
-        --strb_i                              : in  std_logic; -- Valid in
-        wave_data_i                         : in  std_logic_vector((CONFIG_DATA_WIDTH - 1) downto 0);
         wave_config_i                       : in  std_logic_vector(7 downto 0); 
         restart_wave_i                      : in  std_logic;
 
+        -- Pulser
+        pulser_nb_repetitions_value_i       : in  std_logic_vector((NB_POINTS_WIDTH - 1) downto 0);
+        pulser_t1_value_i                   : in  std_logic_vector((TIMER_WIDTH - 1) downto 0);
+        pulser_t2_value_i                   : in  std_logic_vector((TIMER_WIDTH - 1) downto 0);
+        pulser_t3_value_i                   : in  std_logic_vector((TIMER_WIDTH - 1) downto 0);
+        pulser_t4_value_i                   : in  std_logic_vector((TIMER_WIDTH - 1) downto 0);
+        pulser_tdamp_value_i                : in  std_logic_vector((TIMER_WIDTH - 1) downto 0);
+        pulser_config_invert_i              : in  std_logic;
+        pulser_config_triple_i              : in  std_logic;
+
+        -- Cordic 
+        dds_phase_term_value_i              : in  std_logic_vector((PHASE_WIDTH - 1) downto 0);
+        dds_init_phase_value_i              : in  std_logic_vector((PHASE_WIDTH - 1) downto 0);
+        dds_nb_points_i                     : in  std_logic_vector((NB_POINTS_WIDTH - 1) downto 0);
+        dds_nb_repetitions_i                : in  std_logic_vector((NB_POINTS_WIDTH - 1) downto 0);
+        dds_mode_time_i                     : in  std_logic;
+    
         -- Output interface
         strb_o                              : out std_logic;
         wave_data_o                         : out std_logic_vector( (OUTPUT_WIDTH - 1) downto 0);
@@ -47,67 +62,18 @@ end wave_generator;
 
 architecture behavioral of wave_generator is
     
+    
+    
     ---------------
     -- Constants --
-    ---------------    
-
-    -- Wave type
+    ---------------
 
     constant    TYPE_PULSER                     : std_logic_vector := x"1";
     constant    TYPE_CORDIC                     : std_logic_vector := x"2";
+    constant    PULSER_NB_REPETITIONS_WIDTH     : positive         := NB_POINTS_WIDTH;
 
 
-    -- CORDIC
-    constant    CORDIC_PHASE_WIDTH              : positive := PHASE_INTEGER_PART + (-PHASE_FRAC_PART + 1) ; --pkg
-    constant    CORDIC_PHASE_TERM_START         : natural  := 0;
-    constant    CORDIC_PHASE_TERM_END           : natural  := ( CORDIC_PHASE_TERM_START + (CORDIC_PHASE_WIDTH - 1) );
 
-    constant    CORDIC_INIT_PHASE_WIDTH         : positive := CORDIC_PHASE_WIDTH; 
-    constant    CORDIC_INIT_PHASE_START         : natural  := (CORDIC_PHASE_TERM_END + 1);
-    constant    CORDIC_INIT_PHASE_END           : natural  := ( CORDIC_INIT_PHASE_START + (CORDIC_INIT_PHASE_WIDTH - 1) );
-    
-    constant    CORDIC_NB_POINTS_WIDTH          : positive := NB_POINTS_WIDTH; -- pkg
-    constant    CORDIC_NB_POINTS_START          : natural  := (CORDIC_INIT_PHASE_END + 1);
-    constant    CORDIC_NB_POINTS_END            : natural  := ( CORDIC_NB_POINTS_START + (CORDIC_NB_POINTS_WIDTH - 1) );  
-
-    constant    CORDIC_NB_REPETITIONS_WIDTH     : positive := CORDIC_NB_POINTS_WIDTH; --TODO: separate points and repetitions
-    constant    CORDIC_NB_REPETITIONS_START     : natural  := (CORDIC_NB_POINTS_END + 1);
-    constant    CORDIC_NB_REPETITIONS_END       : natural  := ( CORDIC_NB_REPETITIONS_START + (CORDIC_NB_REPETITIONS_WIDTH - 1) ); 
-
-    constant    CORDIC_MODE_TIME_WIDTH          : positive := 1;
-    constant    CORDIC_MODE_TIME_START          : natural  := ( CORDIC_NB_REPETITIONS_END + 1 );
-    constant    CORDIC_MODE_TIME_END            : natural  := ( CORDIC_MODE_TIME_START +  (CORDIC_MODE_TIME_WIDTH - 1) );
-    
-    -- Pulser
-    constant    PULSER_NB_REPETITIONS_WIDTH     : positive := NB_POINTS_WIDTH;
-    constant    PULSER_NB_REPETITIONS_START     : natural  := 0;
-    constant    PULSER_NB_REPETITIONS_END       : natural  := ( PULSER_NB_REPETITIONS_START + (PULSER_NB_REPETITIONS_WIDTH - 1) ); 
-
-    constant    PULSER_TIMER_WIDTH              : positive := TIMER_WIDTH; --pkg
-    constant    PULSER_T1_START                 : natural  := (PULSER_NB_REPETITIONS_END + 1);
-    constant    PULSER_T1_END                   : natural  := ( PULSER_T1_START + (PULSER_TIMER_WIDTH - 1) );
-
-    constant    PULSER_T2_START                 : natural  := ( PULSER_T1_END + 1 );
-    constant    PULSER_T2_END                   : natural  := ( PULSER_T2_START + (PULSER_TIMER_WIDTH - 1) );
-
-    constant    PULSER_T3_START                 : natural  := ( PULSER_T2_END + 1 );
-    constant    PULSER_T3_END                   : natural  := ( PULSER_T3_START + (PULSER_TIMER_WIDTH - 1) );
-
-    constant    PULSER_T4_START                 : natural  := ( PULSER_T3_END + 1 );
-    constant    PULSER_T4_END                   : natural  := ( PULSER_T4_START  + (PULSER_TIMER_WIDTH - 1) );
-    
-    constant    PULSER_TDAMP_START              : natural  := ( PULSER_T4_END + 1 );
-    constant    PULSER_TDAMP_END                : natural  := ( PULSER_TDAMP_START +  (PULSER_TIMER_WIDTH - 1) );
-
-    constant    PULSER_INVERT_PULSER_WIDTH      : positive := 1;
-    constant    PULSER_INVERT_PULSER_START      : natural  := ( PULSER_TDAMP_END + 1 );
-    constant    PULSER_INVERT_PULSER_END        : natural  := ( PULSER_INVERT_PULSER_START +  (PULSER_INVERT_PULSER_WIDTH - 1) );
-    
-    constant    PULSER_TRIPLE_PULSER_WIDTH      : positive := 1;
-    constant    PULSER_TRIPLE_PULSER_START      : natural  := ( PULSER_INVERT_PULSER_END + 1 );
-    constant    PULSER_TRIPLE_PULSER_END        : natural  := ( PULSER_TRIPLE_PULSER_START +  (PULSER_TRIPLE_PULSER_WIDTH - 1) );
-
-    
     -------------
     -- Signals --
     -------------
@@ -117,7 +83,6 @@ architecture behavioral of wave_generator is
     signal bang_reg                           : std_logic;
     signal wave_config                        : std_logic_vector(7 downto 0);
     signal wave_config_reg                    : std_logic_vector(7 downto 0);
-    signal input_wave_data                    : std_logic_vector((CONFIG_DATA_WIDTH - 1) downto 0); 
     signal restart_wave                       : std_logic;
 
     signal wave_type                          : std_logic_vector(3 downto 0);
@@ -129,7 +94,7 @@ architecture behavioral of wave_generator is
     signal cordic_nb_points                   : std_logic_vector((NB_POINTS_WIDTH - 1) downto 0);
     signal cordic_nb_repetitions              : std_logic_vector((NB_POINTS_WIDTH - 1) downto 0);
     signal cordic_initial_phase               : ufixed(PHASE_INTEGER_PART downto PHASE_FRAC_PART);  
-    signal cordic_mode_time                   : std_logic_vector(0 downto 0);
+    signal cordic_mode_time                   : std_logic;
     signal cordic_restart_cycles              : std_logic;
     
     signal cordic_strb_o                      : std_logic;
@@ -142,6 +107,7 @@ architecture behavioral of wave_generator is
     -- Pulser
     signal pulser_bang                        : std_logic; 
     signal pulser_strb_i                      : std_logic; -- Valid in for all inputs and mode interface
+    signal pulser_restart                     : std_logic; -- Valid in for all inputs and mode interface
     signal pulser_nb_repetitions              : std_logic_vector( (PULSER_NB_REPETITIONS_WIDTH - 1) downto 0);
     signal pulser_timer1                      : std_logic_vector( (TIMER_WIDTH - 1) downto 0); 
     signal pulser_timer2                      : std_logic_vector( (TIMER_WIDTH - 1) downto 0);
@@ -149,8 +115,8 @@ architecture behavioral of wave_generator is
     signal pulser_timer4                      : std_logic_vector( (TIMER_WIDTH - 1) downto 0);
     signal pulser_timer_damp                  : std_logic_vector( (TIMER_WIDTH - 1) downto 0);
 
-    signal pulser_invert_pulser               : std_logic_vector(0 downto 0);
-    signal pulser_triple_pulser               : std_logic_vector(0 downto 0); 
+    signal pulser_invert_pulser               : std_logic;
+    signal pulser_triple_pulser               : std_logic; 
     
     signal pulser_strb_o                      : std_logic;
     signal pulser_done                        : std_logic;
@@ -165,10 +131,8 @@ architecture behavioral of wave_generator is
 
 begin
 
-
     -- Input
     bang                <= bang_i;
-    input_wave_data     <= wave_data_i; 
     wave_config         <= wave_config_i;       
     restart_wave        <= restart_wave_i;
 
@@ -180,7 +144,7 @@ begin
     begin
         if (areset_i = '1') then
             bang_reg    <= '0';
-            wave_type   <= (others => '0');
+            wave_config_reg   <= (others => '0');
         elsif (rising_edge(clock_i)) then
 
             bang_reg <= bang;
@@ -208,22 +172,28 @@ begin
     pulser_strb_i          <=               bang_reg       when ( wave_type = TYPE_PULSER)
                                     else    '0';  
 
+    pulser_restart          <=              restart_wave   when ( wave_type = TYPE_PULSER)
+                                    else    '0';   
+
     cordic_strb_i           <=              bang_reg       when ( wave_type = TYPE_CORDIC)
                                     else    '0';         
     
     cordic_restart_cycles  <=               restart_wave   when ( wave_type = TYPE_CORDIC)
                                     else    '0';   
                                     
-                                    
-                                    
-    pulser_nb_repetitions  <= input_wave_data( PULSER_NB_REPETITIONS_END  downto PULSER_NB_REPETITIONS_START ); 
-    pulser_timer1          <= input_wave_data( PULSER_T1_END              downto PULSER_T1_START             );
-    pulser_timer2          <= input_wave_data( PULSER_T2_END              downto PULSER_T2_START             ); 
-    pulser_timer3          <= input_wave_data( PULSER_T3_END              downto PULSER_T3_START             ); 
-    pulser_timer4          <= input_wave_data( PULSER_T4_END              downto PULSER_T4_START             );
-    pulser_timer_damp      <= input_wave_data( PULSER_TDAMP_END           downto PULSER_TDAMP_START          ); 
-    pulser_invert_pulser   <= input_wave_data( PULSER_INVERT_PULSER_END   downto PULSER_INVERT_PULSER_START  );
-    pulser_triple_pulser   <= input_wave_data( PULSER_TRIPLE_PULSER_END   downto PULSER_TRIPLE_PULSER_START  );
+
+    ------------
+    -- Pulser --
+    ------------
+    pulser_bang            <= pulser_strb_i; 
+    pulser_nb_repetitions  <= pulser_nb_repetitions_value_i; 
+    pulser_timer1          <= pulser_t1_value_i;
+    pulser_timer2          <= pulser_t2_value_i; 
+    pulser_timer3          <= pulser_t3_value_i; 
+    pulser_timer4          <= pulser_t4_value_i;
+    pulser_timer_damp      <= pulser_tdamp_value_i; 
+    pulser_invert_pulser   <= pulser_config_invert_i;
+    pulser_triple_pulser   <= pulser_config_triple_i;
 
     wave_pulser: entity work.pulser
     generic map(
@@ -237,6 +207,7 @@ begin
             
             -- Input interface
             strb_i                              => pulser_strb_i,
+            restart_i                           => pulser_restart,
             nb_repetitions_i                    => pulser_nb_repetitions,
             t1_i                                => pulser_timer1,
             t2_i                                => pulser_timer2,
@@ -248,8 +219,8 @@ begin
             bang_i                              => pulser_bang,
             
             -- Mode Interface 
-            invert_pulser_i                     => pulser_invert_pulser(0),
-            triple_pulser_i                     => pulser_triple_pulser(0),
+            invert_pulser_i                     => pulser_invert_pulser,
+            triple_pulser_i                     => pulser_triple_pulser,
             
             -- Output interface
             strb_o                              => pulser_strb_o,
@@ -261,11 +232,11 @@ begin
     --  CORDIC --
     -------------
 
-    cordic_phase_term     <= to_ufixed( input_wave_data( CORDIC_PHASE_TERM_END     downto CORDIC_PHASE_TERM_START     ) , cordic_phase_term) ;
-    cordic_nb_points      <=            input_wave_data( CORDIC_NB_POINTS_END      downto CORDIC_NB_POINTS_START      );
-    cordic_nb_repetitions <=            input_wave_data( CORDIC_NB_REPETITIONS_END downto CORDIC_NB_REPETITIONS_START );
-    cordic_initial_phase  <= to_ufixed( input_wave_data( CORDIC_INIT_PHASE_END     downto CORDIC_INIT_PHASE_START     ) , cordic_initial_phase);
-    cordic_mode_time      <=            input_wave_data( CORDIC_MODE_TIME_END      downto CORDIC_MODE_TIME_START      );
+    cordic_phase_term     <= to_ufixed( dds_phase_term_value_i , cordic_phase_term) ;
+    cordic_nb_points      <= dds_nb_points_i;
+    cordic_nb_repetitions <= dds_nb_repetitions_i;
+    cordic_initial_phase  <= to_ufixed( dds_init_phase_value_i , cordic_initial_phase);
+    cordic_mode_time      <= dds_mode_time_i;
 
     wave_cordic: entity work.dds_cordic
         generic map(
@@ -288,7 +259,7 @@ begin
             initial_phase_i                     => cordic_initial_phase,
             nb_points_i                         => cordic_nb_points,
             nb_repetitions_i                    => cordic_nb_repetitions,
-            mode_time_i                         => cordic_mode_time(0), -- Forced FALSE
+            mode_time_i                         => cordic_mode_time, -- Forced FALSE
             
             -- Control interface
             restart_cycles_i                    => cordic_restart_cycles,
@@ -304,7 +275,7 @@ begin
 
     -- Output
 
-    resized_pulser_data     <= to_slv(to_sfixed( pulser_data,cordic_sine_phase));
+    resized_pulser_data     <= to_slv( to_sfixed( signed(pulser_data), cordic_sine_phase)  ); 
     slv_cordic_sine_phase   <= to_slv(cordic_sine_phase);                                    
 
 
